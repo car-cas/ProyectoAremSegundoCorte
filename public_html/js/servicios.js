bd2e3c795ffd1e2debd33617b9a6e76dc13da257
car-cas <carlos.castaneda-l@mail.escuelaing.edu.co>
2018-10-25 23:05:58 -0500

    pagina

--- public_html/js/servicios.js
+++ public_html/js/servicios.js
@@ -6,23 +6,7 @@
 
 (function requestTasaMonida($) {
     function requestTasa() {
-        endpoint = 'live'
-        access_key = 'e3f4d12f6fc5e01bffe459d94ff544b4';
-
-        from = 'EUR';
-        to = 'GBP';
-        amount = '10';
-        
-        $.ajax({
-            method: 'POST',
-            url: 'http://apilayer.net/api/' + endpoint + '?access_key=' + access_key,
-            dataType: 'jsonp',
-            success: function (json) {
-
-                // access the conversion result in json.result
-                alert(json.result);
-
-            }
-        });
+        var porld=document.getElementById("moneda").value;
+        aler(porld);
     }
-}(jQuerry));
+});
