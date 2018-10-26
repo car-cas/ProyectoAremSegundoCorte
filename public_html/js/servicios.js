2b7cf392e9c996124288afa32dd8b167acb7f30e
unknown <2110540@labinfo.is.escuelaing.edu.co>
2018-10-26 09:46:18 -0500

    tabla

--- public_html/js/servicios.js
+++ public_html/js/servicios.js
@@ -8,8 +8,8 @@ function requestUrl()
     var moneda = document.getElementById("moneda");
     var tipo1 = document.getElementById("despliegue");
     var tipo2 = document.getElementById("despliegue2");
-    url = "https://www.alphavantage.co/query?function=FX_INTRADAY&from_symbol=" 
-         + tipo1.value + "&to_symbol=" + tipo2.value + "&interval=5min&apikey=Q8EQ7DDSA3ZMYPYM";
+    url = "https://www.alphavantage.co/query?function=FX_INTRADAY&from_symbol="
+            + tipo1.value + "&to_symbol=" + tipo2.value + "&interval=5min&apikey=Q8EQ7DDSA3ZMYPYM";
     var arrUrl = url.split("&");
     var urlObj = {};
     var xmlHttp = new XMLHttpRequest();
@@ -28,7 +28,52 @@ function requestUrl()
     return value;
 }
 
-function getResponse(){
- 
+function getResponse() {
+    
+    var moneda = document.getElementById("despleigue3").value;
+    
+    var monedas =  ["JPY", "USD", "COP", "EUR", "BRL", "CAD", "AUD", "CHF", "GBP"];
+    
+    var body = document.getElementsByTagName("body")[0];
+    var tabla = document.createElement("table");
+    var tblBody = document.createElement("tbody");
+    for (var i = 0; i < monedas.length - 1; i++) {
+        
+        var fila = document.createElement("tr");
+        for (var j = 0; j < 2; j++) {
+            if (monedas[i] != moneda) {
+                var columna = document.createElement("td");
+                var celda;
+                if (j == 0) {
+                    celda = document.createTextNode(monedas[i]);
+                } else {
+                    celda = document.createTextNode(read(monedas[i], moneda));
+                }
+                columna.appendChild(celda);
+                fila.appendChild(columna);
+            }
+        }
+        tblBody.appendChild(fila);
+    }
+    
+    tabla.appendChild(tblBody);
+    body.appendChild(tabla);
+    tabla.setAttribute("border", "2");
+}
+
+function read(moneda1, moneda2) {
+    
+    var url = "https://www.alphavantage.co/query?function=FX_INTRADAY&from_symbol=" + moneda1 + "&to_symbol=" + moneda2+"&interval=5min&apikey=Q8EQ7DDSA3ZMYPYM";
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", url, false); // false for synchronous request
+    xmlHttp.send(null);
+    var lista = xmlHttp.responseText;
+    lista = lista.split("\n");
+    var value = lista[15];
+    value = value.split(":");
+    value = value[1];
+    var bal = value.substr(2, value.length - 3);
+    bal = parseFloat(bal);
+    return bal;
 }
 
